1cdef47a3d0c60ee3420a74b41eda72b99ff586f
lshgit <dltlgus090409@naver.com>
2023-05-21 00:16:52 +0900

    계산기 페이지 작성

---
+++ app.js
@@ -0,0 +1,29 @@
+const display = document.getElementById("inputvalue");
+
+function output(inputnumber){
+        display.value = display.value + inputnumber;
+}
+
+function finish(){
+    let fir = document.getElementById("inputvalue");
+    let b = fir.value;
+    let d = eval(b);
+    fir.value = d;
+}
+
+function clearInput(){
+    display.value = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
